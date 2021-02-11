5f8a6707aef9d09d46e81105029fc244a6e6eb82
Frank Brano Gomes <64440458+iamu985@users.noreply.github.com>
2021-02-11 23:26:55 +0530

    added a static method
    added a way to generate hash for nonce value to be included in the merkle root

--- brick.py
+++ brick.py
@@ -10,5 +10,13 @@ class Block:
 		self.nonce = 0
 	
 	def __call__(self):
-		return sha256(str(self.__dict__).encode('ascii')).hexdigest()
+		hashprint = sha256(str(self.__dict__).encode('ascii')).hexdigest()
+		while not hashprint.startswith('0'*2):
+			self.nonce += 1
+			hashprint = sha256(str(self.__dict__).encode('ascii')).hexdigest()
+		return hashprint
+	
+	@staticmethod
+	def genHash(nonce):
+		return sha256(str(nonce).encode('ascii')).hexdigest()
 
